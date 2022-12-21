f5c8c50c8977e20f98c1a4ae326d51d2e54ba952
mathieu.capo <mathieu.capo@teads.com>
2022-12-21 11:23:46 +0100

    correct p1 day4 + p2 day4

--- src/main/scala/com/days/DayFour.scala
+++ src/main/scala/com/days/DayFour.scala
@@ -10,22 +10,34 @@ object DayFour extends App {
     case Array(a, b) => (a, b)
   }
 
-  def splitIntoTuple(ab: (String, String)): ((String,String),(String,String)) = ab match {
+  def splitIntoTuple(ab: (String, String)): ((Int, Int), (Int, Int)) = ab match {
     case (a,b) => val splittedA = a.split("-")
                   val splittedB= b.split("-")
       (
-        (splittedA(0),splittedA(1)),(splittedB(0),splittedB(1))
+        (splittedA(0).toInt,splittedA(1).toInt),(splittedB(0).toInt,splittedB(1).toInt)
       )
   }
 
-  def isContained(tuple: ((String, String), (String, String))) : Int = tuple match {
-    case (a,b) =>  if(a._1 <= b._1 && a._2 >= b._2) 1
-              else if(b._1 <= a._1 && b._2 >= a._2) 1
+  def isContained(tuple: ((Int, Int), (Int, Int))) : Int = tuple match {
+    case (a, b) => if (a._1 <= b._1 && a._2 >= b._2) 1
+              else if (b._1 <= a._1 && b._2 >= a._2) 1
               else 0
   }
+
+  def overlaps(tuple: ((Int, Int), (Int, Int))): Int = tuple match {
+    case (a, b) => if ( (Range.inclusive(a._1, a._2, 1) contains b._1) 
+                     || (Range.inclusive(b._1, b._2, 1) contains a._1)) 1
+              else if ( (Range.inclusive(a._1, a._2, 1) contains b._2)
+                     || (Range.inclusive(b._1, b._2, 1) contains a._2)) 1
+              else 0
+  }
+
+
   val mListWithGroupAndTupleRange = mListSeparateGroups.map(splitIntoTuple)
   val isPlusGrand = mListWithGroupAndTupleRange.map(groupe => isContained(groupe))
+  val isOverlapping = mListWithGroupAndTupleRange.map(groupe => overlaps(groupe))
   println(mListWithGroupAndTupleRange)
   println(isPlusGrand.sum)
+  println(isOverlapping.sum)
 
 }
